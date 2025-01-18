6b327203db11eb8204e5cac808d34efd185c088c
Arthur63 <arthur63@163.com>
2025-01-18 16:07:25 +0800

    add objectbox

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_linux
   file_selector_linux
+  objectbox_flutter_libs
   url_launcher_linux
 )
 
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_windows
   file_selector_windows
+  objectbox_flutter_libs
   share_plus
   url_launcher_windows
 )
